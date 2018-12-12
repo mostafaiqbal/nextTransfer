dd6ca34103ad764ca10728ba0154622d214d0a79
Mostafa Iqbal <rashel.edu@gmail.com>
2018-12-12 18:02:52 +0000

    'section'

--- assets/scripts/miqJs.js
+++ assets/scripts/miqJs.js
@@ -1,1 +1,14 @@
 // miq JS by Mostafa Iqbal
+
+$(document).ready(function(){
+    $(window).scroll(function(){
+        var scroll = $(window).scrollTop();
+        if (scroll > 200) {
+          $("#navWrap").css("background" , "#000000");
+        } else if (scroll < 200) {
+            $("#navWrap").css("background" , "");
+        } else{
+            $(".navWrap").css("background" , "");  	
+        }
+    })
+  })
